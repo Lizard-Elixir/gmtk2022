bc6de49498b9bb8917564093ca2bd3c45d33c88a
Katherine Russell <71149848+KatRuss@users.noreply.github.com>
2022-07-16 11:37:54 +0100

    Comment on possible AI solution

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -30,6 +30,18 @@ public class EnemyAI : MonoBehaviour {
 		};
 
 		// TODO: prioritize using something if it's likely to overshoot next turn
+		// (KAT: Calcualte average values for each roll/combinations of rolls.
+		//		If the average is over the threshold for that bar, then choose a lower avg. die.
+		//		If all dice are over that threshold, then choose a d4 and recognise it's going to overflow)
+		// (KAT: Averages:
+		//			d4: 2
+		//			d6: 3
+		//			d8: 4
+		//			d4+d6: 6
+		//			d4+d8: 7
+		//			d6+d8: 8
+		//			d6+d8+d4: 10)
+		
 
 		// calculate the sum of biases
 		// if it's not higher than the threshold of action,
